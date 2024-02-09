883411a8db6e3d0e10a63ab52d4e691f9087aed4
KrystianRusin <krystian.r@hotmail.com>
2024-02-09 16:17:45 -0500

    changed  color scheme and background

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,10 +3,20 @@ import Nav from "./components/Nav";
 import Intro from "./components/Intro";
 import Socials from "./components/Socials";
 import About from "./components/About";
+import Starfield from "react-starfield";
 
 function App() {
   return (
     <div className="content-container">
+      <Starfield
+        style={{
+          position: "absolute",
+          width: "100%",
+          height: "100%",
+          speedFactor: 0.05,
+          zIndex: -1,
+        }}
+      />
       <Nav className="nav" />
       <div className="main-content">
         <div className="center-content">
