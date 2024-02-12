849f6c4c8398f01056a31359b8f51869fbd8b73c
Krystian Rusin <36743674+KrystianRusin@users.noreply.github.com>
2024-02-11 22:30:21 -0500

    contact me sends inquiry email

--- client/src/components/ContactModal.jsx
+++ client/src/components/ContactModal.jsx
@@ -1,13 +1,51 @@
 import "../styles/ContactModal.css";
+import React, { useRef } from "react"; // Import useRef
 
 const ContactModal = ({ onClose }) => {
+  const formRef = useRef();
+
   const stopPropagation = (event) => {
     event.stopPropagation();
   };
 
+  const resetForm = () => {
+    formRef.current.reset();
+  };
+
+  const handleSubmit = (event) => {
+    onClose();
+    event.preventDefault();
+    fetch("http://localhost:5000/send", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name: event.target.name.value,
+        email: event.target.email.value,
+        message: event.target.message.value,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.status === "success") {
+          alert("Message Sent.");
+          resetForm();
+        } else if (data.status === "fail") {
+          alert("Message failed to send.");
+          onClose();
+        }
+      });
+  };
+
   return (
     <div className="modal" onClick={onClose}>
-      <form className="contact-form" onClick={stopPropagation}>
+      <form
+        ref={formRef}
+        className="contact-form"
+        onClick={stopPropagation}
+        onSubmit={handleSubmit}
+      >
         <h2>Contact Me!</h2>
         <div className="input-container">
           <label>Name:</label>
---
+++ server/server.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const nodemailer = require("nodemailer");
+require("dotenv").config();
+const app = express();
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.json());
+
+app.post("/send", async (req, res) => {
+  let { name, email, message } = req.body;
+  console.log(process.env.EMAIL_NAME);
+  console.log(process.env.EMAIL_PASS);
+
+  let transporter = nodemailer.createTransport({
+    service: "gmail",
+    secure: true,
+    auth: {
+      user: process.env.EMAIL_NAME,
+      pass: process.env.EMAIL_PASS,
+    },
+  });
+
+  let mailOptions = {
+    from: email,
+    to: process.env.EMAIL_RECEIVER,
+    subject: `Message from ${name}`,
+    text: message,
+  };
+
+  transporter.sendMail(mailOptions, (err, data) => {
+    if (err) {
+      res.json({
+        status: "fail",
+      });
+      console.log(err);
+    } else {
+      res.json({
+        status: "success",
+      });
+    }
+  });
+});
+
+app.listen(5000, () => {
+  console.log("Server is running on port 5000");
+});
