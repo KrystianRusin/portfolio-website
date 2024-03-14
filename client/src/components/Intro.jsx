9514fd95daf37907829ca63134c09621ad7cf6bc
Krystian Rusin <36743674+KrystianRusin@users.noreply.github.com>
2024-03-14 11:55:24 -0400

    added animations for intro

--- client/src/components/Intro.jsx
+++ client/src/components/Intro.jsx
@@ -1,6 +1,7 @@
 import { Typewriter } from "react-simple-typewriter";
 import meImage from "../assets/mainIconsDark.svg";
 import "../styles/Intro.css";
+import { motion } from "framer-motion";
 
 const Intro = () => {
   const roles = [
@@ -12,12 +13,20 @@ const Intro = () => {
   return (
     <div className="intro-container">
       <div className="intro-text-container">
-        <h2>
+        <motion.h2
+          initial={{ x: -100, opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
+          transition={{ type: "tween", duration: 0.5, delay: 0.5 }}
+        >
           Hi, My name is
           <br />
           <span className="name-title">Krystian Rusin</span>
-        </h2>
-        <h3>
+        </motion.h2>
+        <motion.h3
+          initial={{ x: -100, opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
+          transition={{ delay: 0.5, duration: 0.5 }}
+        >
           I&apos;m {"{  "}
           <span>
             <Typewriter
@@ -32,17 +41,35 @@ const Intro = () => {
             />
           </span>
           {" }"}
-        </h3>
-        <p className="intro-info">
+        </motion.h3>
+        <motion.p
+          className="intro-info"
+          initial={{ x: "-100vw" }}
+          animate={{ x: 0 }}
+          transition={{ type: "tween", duration: 0.5, delay: 0.8 }}
+        >
           Based out of Toronto, Ontario with experience in web development, and
           software development. I am working for ObjectNova Inc. as a Full Stack
           Engineer.
-        </p>
-        <a className="download-a" href="/resume.pdf" target="_blank">
+        </motion.p>
+        <motion.a
+          className="download-a"
+          href="/resume.pdf"
+          target="_blank"
+          initial={{ x: "-100vw" }}
+          animate={{ x: 0 }}
+          transition={{ type: "tween", duration: 0.5, delay: 1.1 }}
+        >
           <button className="download-btn">View my Resume!</button>
-        </a>
+        </motion.a>
       </div>
-      <img src={meImage} alt="PLACEHOLDER" className="me-img" />
+      <motion.div
+        initial={{ x: 100, opacity: 0 }}
+        animate={{ x: 0, opacity: 1 }}
+        transition={{ type: "tween", duration: 0.5, delay: 1.1 }}
+      >
+        <img src={meImage} alt="PLACEHOLDER" className="me-img" />
+      </motion.div>
     </div>
   );
 };
